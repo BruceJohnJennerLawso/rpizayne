771303bfef3af49b9a3f9f28cb6a6dc94c8f0a5b
bruce <johnnybgoode@rogers.com>
2016-12-17 15:43:10 -0500

    made pin 25 move the cursor up 5 ticks

--- monitor.py
+++ monitor.py
@@ -9,7 +9,7 @@
 import wiringpi
 import sys
 import time
-
+import uinput
 
 def clearTerminal(x):
 	## Clears the screen after printing x newlines.
@@ -23,13 +23,21 @@ if(__name__ == "__main__"):
 	wiringpi.wiringPiSetup()
 	pinsRange = [25, 24, 23, 22, 21]
 	
+	
+	events = (uinput.REL_X, uinput.REL_Y, uinput.BTN_LEFT, uinput.BTN_RIGHT,)
+	
 	for i in pinsRange:
 		wiringpi.pinMode(i, 0)
 	## sets GPIO 1 to input
 
-	while(True):
-		for i in pinsRange:
-			print "%i: %r" % (i, wiringpi.digitalRead(i))
-		time.sleep(1)
-		sys.stdout.write("\x1b[2J\x1b[H");
 
+	with uinput.Device(events) as device:
+		while(True):
+			for i in pinsRange:
+				print "%i: %r" % (i, wiringpi.digitalRead(i))
+			time.sleep(0.1)
+			
+			if(wiringpi.digitalRead(25)):
+				device.emit(uinput.REL_Y, 5)
+			##device.emit(uinput.REL_X, 5, syn=False)
+			sys.stdout.write("\x1b[2J\x1b[H");
